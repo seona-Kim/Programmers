a5d93cab3f786b67e5021349bae38a5002acfc3b
김선아 <149765577+seona-Kim@users.noreply.github.com>
2024-05-12 22:24:24 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.01 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int n) {
+        int size = n / 2 + n % 2;
+        int[] answer = new int[size];
+        for (int i = 0; i < size; i++) {
+            answer[i] = i * 2 + 1;
+        }
+        return answer;
+    }
+}
